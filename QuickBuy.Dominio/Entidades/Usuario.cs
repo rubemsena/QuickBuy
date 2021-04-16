518ec93fc00da17d6b1f2a9879c8f6d1762f9210
Rubem Sena <rubemsena@yahoo.com.br>
2021-04-16 17:12:56 -0300

    Criação das entidades

---
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    class Usuario
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+
+        /// <summary>
+        /// Um usuario pode ter nenhum ou vários pédidos
+        /// </summary>
+        public ICollection<Pedido> Pedidos { get; set; }
+
+    }
+}
